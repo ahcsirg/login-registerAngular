6d1c7fec7e34e476a6cc4188dc21605af95ff2a1
Alexis Santander <a.santander.t@gmail.com>
2024-07-08 17:26:29 -0400

    agrega Oninit

--- src/app/auth/recovery-password/recovery-password.component.ts
+++ src/app/auth/recovery-password/recovery-password.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recovery-password',
   templateUrl: './recovery-password.component.html',
   styleUrl: './recovery-password.component.css'
 })
-export class RecoveryPasswordComponent {
+export class RecoveryPasswordComponent implements OnInit{
+    public name!: string 
 
+    ngOnInit(): void {
+     this.name = "asdfasdfasdf";
+
+    }
+
+    
+
+
+    
 }
